5e5cd54695263d748aebb91c58966562ff5939c8
RazmikMkrtchyan <razar.92@gmail.com>
2016-06-14 11:46:16 +0400

    Update IgniteTest1.java

--- src/main/java/igniteTest/IgniteTest1.java
+++ src/main/java/igniteTest/IgniteTest1.java
@@ -1,23 +1,20 @@
 package igniteTest;
 
-
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteCompute;
 
-import org.apache.ignite.lang.IgniteRunnable;
 /**
  * Created by Razmik.Mkrtchyan on 5/30/2016.
  */
 public class IgniteTest1 {
 
 	public static void main(String[] args) {
-               Ignite ignite=Ignition.start("ignite-config.xml");
+		Ignite ignite=Ignition.start("ignite-config.xml");
 		IgniteCache<Integer,Integer> cache= ignite.getOrCreateCache("test1");
 		cache.put(1,1);
-	
+
 	}
 
 }
