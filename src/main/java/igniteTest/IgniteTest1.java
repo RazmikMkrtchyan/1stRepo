ca2a65366fbe835bbbf64a8e007ebe61c5a5da33
RazmikMkrtchyan <razar.92@gmail.com>
2016-06-16 11:16:57 +0400

    Update IgniteTest1.java

--- src/main/java/igniteTest/IgniteTest1.java
+++ src/main/java/igniteTest/IgniteTest1.java
@@ -13,6 +13,8 @@ public class IgniteTest1 {
 	public static void main(String[] args) {
 		Ignite ignite=Ignition.start("ignite-config.xml");
 		IgniteCache<Integer,Integer> cache= ignite.getOrCreateCache("test1");
+		cache.put(2,2);
+		IgniteCache<Integer,Integer> cache= ignite.getOrCreateCache("test2");
 		cache.put(1,1);
 
 	}
