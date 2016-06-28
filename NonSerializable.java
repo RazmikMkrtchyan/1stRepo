4ff573fabe2e3e315409244798587e00161bcb95
RazmikMkrtchyan <razar.92@gmail.com>
2016-06-28 16:54:56 +0400

    Add files via upload

---
+++ NonSerializable.java
@@ -0,0 +1,12 @@
+package igniteTest;
+
+/**
+ * Created by Razmik.Mkrtchyan on 6/28/2016.
+ */
+public class NonSerializable {
+    private String string;
+
+    public NonSerializable(String string) {
+        this.string = string;
+    }
+}
