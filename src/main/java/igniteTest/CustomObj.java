7fcf9c35b88c98c26e2362196b16dc4d3e0e7ac2
RazmikMkrtchyan <razar.92@gmail.com>
2016-06-28 16:51:01 +0400

    Update CustomObj.java

--- src/main/java/igniteTest/CustomObj.java
+++ src/main/java/igniteTest/CustomObj.java
@@ -4,4 +4,9 @@ package igniteTest;
  * Created by Razmik.Mkrtchyan on 6/28/2016.
  */
 public class CustomObj {
+    private NonSerializable nonSerializable;
+
+    public CustomObj(NonSerializable nonSerializable) {
+        this.nonSerializable = nonSerializable;
+    }
 }
