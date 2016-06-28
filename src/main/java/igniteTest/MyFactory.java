8115d1ffa84acd673f22040d4dc44405ada39f01
RazmikMkrtchyan <razar.92@gmail.com>
2016-06-28 15:55:26 +0400

    Update MyFactory.java

--- src/main/java/igniteTest/MyFactory.java
+++ src/main/java/igniteTest/MyFactory.java
@@ -7,8 +7,9 @@ import java.io.Serializable;
  * Created by Razmik on 6/27/2016.
  */
 public class MyFactory implements Factory<MyLoader>,Serializable{
+
     @Override
     public MyLoader create() {
-        return new MyLoader();
+        return new MyLoader(new CustomObj());
     }
 }
